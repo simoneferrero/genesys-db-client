5c580d33aadbd4aec2f7237074129644ca964109
Simone Ferrero <simoneferrero@Stefanias-MacBook-Air.local>
2019-04-08 18:34:07 +0100

    pc-sheet - Added saga to fetch PC data

--- src/sagas/playersCharacters/index.js
+++ src/sagas/playersCharacters/index.js
@@ -3,10 +3,19 @@ import uri from 'urijs'
 
 import { all, call, put, takeLatest } from 'redux-saga/effects'
 
-import { GET_PLAYERS_CHARACTERS } from 'actions/playersCharacters/constants'
 import {
+  // getPlayersCharacters
+  GET_PLAYERS_CHARACTERS,
+  // getPlayerCharacter
+  GET_PLAYER_CHARACTER,
+} from 'actions/playersCharacters/constants'
+import {
+  // getPlayersCharacters
   getPlayersCharactersError,
   getPlayersCharactersSuccess,
+  // getPlayerCharacter
+  getPlayerCharacterSuccess,
+  getPlayerCharacterError,
 } from 'actions/playersCharacters'
 
 import { API_PATH, API_SEGMENTS, REST_METHODS } from 'utils/definitions'
@@ -33,6 +42,31 @@ export function* getPlayersCharactersWatcher() {
   yield takeLatest(GET_PLAYERS_CHARACTERS, getPlayersCharactersSaga)
 }
 
+export function* getPlayerCharacterSaga({ payload: { id } }) {
+  const requestUrl = uri(API_PATH)
+    .segment([API_SEGMENTS.PLAYERS_CHARACTERS, id])
+    .toString()
+
+  const opts = {
+    method: REST_METHODS.GET,
+    url: requestUrl,
+  }
+  try {
+    const response = yield call(axios, opts)
+
+    yield put(getPlayerCharacterSuccess(id, response.data))
+  } catch (error) {
+    yield put(getPlayerCharacterError(id, error))
+  }
+}
+
+export function* getPlayerCharacterWatcher() {
+  yield takeLatest(GET_PLAYER_CHARACTER, getPlayerCharacterSaga)
+}
+
 export default function* rootSaga() {
-  yield all([call(getPlayersCharactersWatcher)])
+  yield all([
+    call(getPlayersCharactersWatcher),
+    call(getPlayerCharacterWatcher),
+  ])
 }
--- src/sagas/playersCharacters/tests/index.test.js
+++ src/sagas/playersCharacters/tests/index.test.js
@@ -2,26 +2,43 @@ import { all, call, put, takeLatest } from 'redux-saga/effects'
 import axios from 'axios'
 
 import rootSaga, {
+  // getPlayersCharacters
   getPlayersCharactersSaga,
   getPlayersCharactersWatcher,
+  // getPlayerCharacter
+  getPlayerCharacterSaga,
+  getPlayerCharacterWatcher,
 } from '../index'
-import { GET_PLAYERS_CHARACTERS } from 'actions/playersCharacters/constants'
 import {
+  // getPlayersCharacters
+  GET_PLAYERS_CHARACTERS,
+  // getPlayerCharacter
+  GET_PLAYER_CHARACTER,
+} from 'actions/playersCharacters/constants'
+import {
+  // getPlayersCharacters
+  getPlayersCharacters,
   getPlayersCharactersError,
   getPlayersCharactersSuccess,
+  // getPlayerCharacter
+  getPlayerCharacter,
+  getPlayerCharacterError,
+  getPlayerCharacterSuccess,
 } from 'actions/playersCharacters'
 
 import { apiPath } from 'mocks'
-import { playersCharactersResponse } from 'mocks/playersCharacters'
+import {
+  playerCharacter1Id,
+  // TODO: use full player character mock
+  playerCharacterSummary1Response,
+  playersCharactersResponse,
+} from 'mocks/playersCharacters'
 import { genericError } from 'mocks/errors'
-
-const action = {
-  type: GET_PLAYERS_CHARACTERS,
-  payload: {},
-}
 
 describe('playersCharacters sagas', () => {
   describe('getPlayersCharacters', () => {
+    const action = getPlayersCharacters()
+
     describe('getPlayersCharactersSaga', () => {
       let generator
 
@@ -77,12 +94,74 @@ describe('playersCharacters sagas', () => {
     })
   })
 
+  describe('getPlayerCharacterSaga', () => {
+    const id = `${playerCharacter1Id}`
+    const action = getPlayerCharacter(id)
+
+    describe('getPlayerCharacterSaga', () => {
+      let generator
+
+      beforeEach(() => {
+        generator = getPlayerCharacterSaga(action)
+      })
+
+      it('should dispatch the correct actions on success', () => {
+        const opts = {
+          method: 'GET',
+          url: `${apiPath}/players-characters/${id}`,
+        }
+        const callAxiosDescriptor = generator.next().value
+        const expectedCallAxiosDescriptor = call(axios, opts)
+        expect(callAxiosDescriptor).toEqual(expectedCallAxiosDescriptor)
+
+        const response = {
+          data: playerCharacterSummary1Response,
+        }
+        const putSuccessDescriptor = generator.next(response).value
+        const expectedPutSuccessDescriptor = put(
+          getPlayerCharacterSuccess(id, playerCharacterSummary1Response),
+        )
+        expect(putSuccessDescriptor).toEqual(expectedPutSuccessDescriptor)
+      })
+
+      it('should dispatch the correct actions on error', () => {
+        generator.next().value
+        const putErrorDescriptor = generator.throw(genericError).value
+        const expectedPutErrorDescriptor = put(
+          getPlayerCharacterError(id, genericError),
+        )
+        expect(putErrorDescriptor).toEqual(expectedPutErrorDescriptor)
+      })
+
+      afterEach(() => {
+        const endGeneratorDescriptor = generator.next().value
+        expect(endGeneratorDescriptor).toBeUndefined()
+      })
+    })
+
+    describe('getPlayerCharacterWatcher', () => {
+      it('should listen for the correct action', () => {
+        const generator = getPlayerCharacterWatcher(action)
+
+        const takeLatestDescriptor = generator.next().value
+        const expectedTakeLatestDescriptor = takeLatest(
+          GET_PLAYER_CHARACTER,
+          getPlayerCharacterSaga,
+        )
+        expect(takeLatestDescriptor).toEqual(expectedTakeLatestDescriptor)
+      })
+    })
+  })
+
   describe('rootSaga', () => {
     it('should export the correct root', () => {
       const generator = rootSaga({})
 
       const allDescriptor = generator.next().value
-      const expectedAllDescriptor = all([call(getPlayersCharactersWatcher)])
+      const expectedAllDescriptor = all([
+        call(getPlayersCharactersWatcher),
+        call(getPlayerCharacterWatcher),
+      ])
       expect(allDescriptor).toEqual(expectedAllDescriptor)
     })
   })
